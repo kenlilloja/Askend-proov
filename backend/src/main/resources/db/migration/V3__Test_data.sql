71fe66fae6a2f576c41159ca216440e11820ba7f
Ken Lilloja <klillo@ttu.ee>
2024-07-18 18:08:32 +0300

    Some small fixes

--- backend/src/main/resources/db/migration/V3__Test_data.sql
+++ backend/src/main/resources/db/migration/V3__Test_data.sql
@@ -16,8 +16,34 @@ VALUES
         (SELECT id FROM filter WHERE name = 'Test Filter12'),
         (SELECT code FROM classifier WHERE code = 'CRITERIA_TYPE_AMOUNT'),
         (SELECT code FROM classifier WHERE code = 'CRITERIA_COMPARISON_MORE'),
-        '1',
+        '5200',
         false,
         now(),
         now()
     );
+
+
+INSERT INTO filter (name,  created_at, modified_at) VALUES ('Filter for testing', now(), now());
+
+
+INSERT INTO criteria (filter_id, criteria_type, condition, value, is_selected, created_at, modified_at)
+VALUES
+    (
+        (SELECT id FROM filter WHERE name = 'Filter for testing'),
+        (SELECT code FROM classifier WHERE code = 'CRITERIA_TYPE_TITLE'),
+        (SELECT code FROM classifier WHERE code = 'CRITERIA_COMPARISON_STARTS_WITH'),
+        'Tere',
+        true,
+        now(),
+        now()
+    ),
+    (
+        (SELECT id FROM filter WHERE name = 'Filter for testing'),
+        (SELECT code FROM classifier WHERE code = 'CRITERIA_TYPE_TITLE'),
+        (SELECT code FROM classifier WHERE code = 'CRITERIA_COMPARISON_ENDS_WITH'),
+        'Tulemast',
+        false,
+        now(),
+        now()
+    );
+
